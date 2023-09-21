ae4e8e71a2954b79b93365a3e2e3fc7982e844b4
Dyplay <marcojasnay2@gmail.com>
2023-09-21 22:40:17 +0200

    update #1

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>✨ Coding Projects</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/dyplay',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/dyplay',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
