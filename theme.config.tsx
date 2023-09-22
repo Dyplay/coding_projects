de335cf4727f4e6cc0cfd9095ea70bcc30c82cf5
Dyplay <marcojasnay2@gmail.com>
2023-09-22 22:36:51 +0200

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -12,7 +12,7 @@ const config: DocsThemeConfig = {
   chat: {
     link: 'https://discord.gg/dyplay',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/dyplay/coding_projects',
   footer: {
     text: 'Made with ❤️ by Dyplay 2023© & Next.js Vercel',
   },
